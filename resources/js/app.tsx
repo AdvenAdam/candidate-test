9dddf7bedd21c09f467b6cfdef73eee8f686d58e
AdvenAdam <advenadam@student.uns.ac.id>
2025-06-10 23:34:33 +0700

    update tsx vite

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,22 +4,22 @@ import './bootstrap';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
+import React from 'react';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.jsx`,
-            import.meta.glob('./Pages/**/*.jsx'),
-        ),
-    setup({ el, App, props }) {
-        const root = createRoot(el);
-
-        root.render(<App {...props} />);
-    },
-    progress: {
-        color: '#4B5563',
-    },
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) =>
+    resolvePageComponent(
+      `./Pages/${name}.tsx`,
+      import.meta.glob<React.ComponentType>('./Pages/**/*.tsx')
+    ),
+  setup({ el, App, props }) {
+    const root = createRoot(el as HTMLElement);
+    root.render(<App {...props} />);
+  },
+  progress: {
+    color: '#4B5563',
+  },
 });
